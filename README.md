26b7d11f32072c3945c5e9278d303af3cc7cc119
marodere <marodere@up4k.org>
2017-07-25 01:44:15 +0300

    support for cidr in blacklist, support for persistent vpn routing table

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ maintain routing table based on RKN registry
 * * *
 `route.sh` is the script that builds routing table based on list of ip addresses. It *should* be specified as the "route-up" openvpn parameter but also it can be executed alone (manually, by cron, etc) For the first time it *should* be executed from openvpn to parse nameservers and gateway from envinroment and store it for further use.
 * * *
-I use two routing tables: vpn0 and vpn1. I use "ip rule add/del" to switch between them. To use the script you *should* define them like this:
+I use three routing tables: `vpn`, `vpn0` and `vpn1`. I use "ip rule add/del" to switch between `vpn{0,1}`. Table `vpn` always have a default route via vpn. To use the script as is you *should* define them like this:
 
-    echo "1000    vpn0" >> /etc/iproute2/rt_tables
-    echo "1001    vpn1" >> /etc/iproute2/rt_tables
+    echo "1000    vpn" >> /etc/iproute2/rt_tables
+    echo "1001    vpn0" >> /etc/iproute2/rt_tables
+    echo "1002    vpn1" >> /etc/iproute2/rt_tables
 
 * * *
 `fetch_blacklist.sh` can work under any user that have an appropriate file permissions `route.sh` *should* be executed as root to perform `ip(8)` calls.
--- bin/route.sh
+++ bin/route.sh
@@ -6,19 +6,37 @@ logfile=${vardir}/log/route.log
 
 function valid_ip() {
 	local err=0 glob_err=0
-	local ip ip_c
+	local ip ip_c mask check_link_local=0
+
+	if [ "$1" == "--check-link-local" ]; then
+		check_link_local=1
+		shift
+	fi
+
+	function is_loopback() {
+		[[ $1 =~ ^127\. ]]
+	}
+
+	function is_link_local() {
+		[[ $1 =~ ^192\.168 ]] || \
+		[[ $1 =~ ^10\. ]] || \
+		[[ $1 =~ ^172\.16\.(1|2|30|31) ]]
+	}
 
 	for ip in $@; do
 		err=0
-		if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+		if [[ $ip =~ ^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(/[0-9]{1,2})?$ ]]; then
 			OIFS=$IFS
 			IFS='.'
-			ip_c=($ip)
+			ip_c=(${BASH_REMATCH[1]})
 			IFS=$OIFS
+			mask=${BASH_REMATCH[2]:-/32}
+			mask=${mask##/}
+			[[ ${mask} -ge 16 && ${mask} -le 32 ]] && \
 			[[ ${ip_c[0]} -le 255 && ${ip_c[1]} -le 255 \
 				&& ${ip_c[2]} -le 255 && ${ip_c[3]} -le 255 ]] && \
-				! [[ $ip =~ ^192\.168 ]] && \
-				! [[ $ip =~ ^127\. ]] || err=$? 
+				! is_loopback $ip && \
+				! ([ $check_link_local == "1" ] && is_link_local $ip) || err=$? 
 		else
 			err=1
 		fi
@@ -41,15 +59,17 @@ function get_new_rt_table() {
 function make_rt_table_active() {
 	log "[I] activating rt_table \"%s\"" "${1}"
 	(set -x; /bin/ip rule add lookup ${1})
-	for current_table in $(/bin/ip rule sh | grep vpn | grep -v ${1} | awk '{ print $NF }' || true); do
-		(set -x; /bin/ip rule del lookup ${current_table})
+	for current_table in $(/bin/ip rule sh | grep vpn | awk '{ print $NF }' || true); do
+		if [ ${current_table} != ${1} -a ${current_table} != "vpn" ]; then
+			(set -x; /bin/ip rule del lookup ${current_table})
+		fi
 	done
 }
 
 function add_route() {
 	local addr="$1"
 	local gw="$2"
-	if ! valid_ip "${addr}" "${gw}"; then
+	if ! valid_ip --check-link-local "${addr}"; then
 		log "[W] add_route failed"
 		return 1
 	fi
@@ -62,11 +82,17 @@ function store_gw() {
 		log "[E] suspicious route, bailing out"
 		return 1
 	fi
-	printf "%s\n%s" "route_vpn_gateway=${route_vpn_gateway}" "dev=${dev}" > ${gwfile}
+	printf "%s\n%s\n" "route_vpn_gateway=${route_vpn_gateway}" "dev=${dev}" > ${gwfile}
 }
 
 function get_gw() {
-	cat ${gwfile}
+	. ${gwfile}
+}
+
+function add_service_rules() {
+	if valid_ip "${ifconfig_local}"; then
+		/bin/ip rule add from "${ifconfig_local}" lookup vpn
+	fi
 }
 
 function add_service_routes() {
@@ -77,10 +103,11 @@ function add_service_routes() {
 		eval "mask=\${route_netmask_${i}:-}"
 		eval "gw=\${route_gateway_${i}:-}"
 		[ -n "${net}" -a -n "${mask}" -a -n "${gw}" ] || break
-		valid_ip "${net}" "${mask}" "${gw}"
-		(set -x; /bin/route add -net ${net} netmask ${mask} gw ${gw} dev ${dev})
+		valid_ip "${net}/${mask}" "${gw}"
+		(set -x; /bin/ip route add "${net}/${mask}" via "${gw}" dev "${dev}")
 		(( ++i ))
 	done
+	(set -x; /bin/ip route add default via "${route_vpn_gateway}" dev "${dev}" table vpn)
 }
 
 function add_nameservers() {
@@ -125,6 +152,7 @@ if [ -n "${route_vpn_gateway:-}" ]; then
 	# probably running from openvpn
 	store_gw
 	add_service_routes
+	add_service_rules
 	add_nameservers
 else
 	# probably running from crontab
@@ -138,7 +166,7 @@ fi
 new_rt_table=$(get_new_rt_table)
 log "[I] deal with rt_table %s" "${new_rt_table}"
 
-eval $(get_gw)
+get_gw
 /bin/ip route flush table ${new_rt_table}
 /bin/ip route add ${route_vpn_gateway} dev ${dev} table ${new_rt_table}
 build_blacklist_routing_table
